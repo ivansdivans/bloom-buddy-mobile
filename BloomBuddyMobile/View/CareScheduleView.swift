7674c7981eadda68fddd0ed76fc7d08cd9c48820
Ivans Mihailovs <mihailovs.ivans@gmail.com>
2024-03-10 20:14:51 +0100

    Add three screens for tabview - careschedule, household and settings

---
+++ BloomBuddyMobile/View/CareScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  CareScheduleView.swift
+//  BloomBuddyMobile
+//
+//  Created by Ivans Mihailovs on 10/03/2024.
+//
+
+import SwiftUI
+
+struct CareScheduleView: View {
+    var body: some View {
+        Text("My Care Schedule")
+    }
+}
+
+#Preview {
+    CareScheduleView()
+}
---
+++ BloomBuddyMobile/View/HouseholdView.swift
@@ -0,0 +1,18 @@
+//
+//  HouseholdView.swift
+//  BloomBuddyMobile
+//
+//  Created by Ivans Mihailovs on 10/03/2024.
+//
+
+import SwiftUI
+
+struct HouseholdView: View {
+    var body: some View {
+        Text("My Household")
+    }
+}
+
+#Preview {
+    HouseholdView()
+}
