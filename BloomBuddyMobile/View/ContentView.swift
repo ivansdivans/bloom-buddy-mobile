56781c3b20d47501e5e7110e72286f940d7c0e62
Daan <daan.brocatus@outlook.com>
2024-03-14 18:56:56 +0100

    Remove template code

--- BloomBuddyMobile/View/ContentView.swift
+++ BloomBuddyMobile/View/ContentView.swift
@@ -9,14 +9,6 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-//    var modelContext: ModelContext
-//    @Query private var items: [Item]
-    
-//    init(modelContext: ModelContext) {
-//        let viewModel = ViewModel(modelContext: modelContext)
-//        _viewModel = State(initialValue: viewModel)
-//    }
-    
     var body: some View {
         TabView {
             CareScheduleView()
@@ -33,52 +25,4 @@ struct ContentView: View {
                 }
         }
     }
-    
-    // MARK: body implementation for SwiftData
-    //    var body: some View {
-    //        NavigationSplitView {
-    //            List {
-    //                ForEach(items) { item in
-    //                    NavigationLink {
-    //                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-    //                    } label: {
-    //                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-    //                    }
-    //                }
-    //                .onDelete(perform: deleteItems)
-    //            }
-    //#if os(macOS)
-    //            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-    //#endif
-    //            .toolbar {
-    //#if os(iOS)
-    //                ToolbarItem(placement: .navigationBarTrailing) {
-    //                    EditButton()
-    //                }
-    //#endif
-    //                ToolbarItem {
-    //                    Button(action: addItem) {
-    //                        Label("Add Item", systemImage: "plus")
-    //                    }
-    //                }
-    //            }
-    //        } detail: {
-    //            Text("Select an item")
-    //        }
-    //    }
-    //
-    //    private func addItem() {
-    //        withAnimation {
-    //            let newItem = Item(timestamp: Date())
-    //            modelContext.insert(newItem)
-    //        }
-    //    }
-    //
-    //    private func deleteItems(offsets: IndexSet) {
-    //        withAnimation {
-    //            for index in offsets {
-    //                modelContext.delete(items[index])
-    //            }
-    //        }
-    //    }
 }
