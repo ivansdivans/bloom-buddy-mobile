baa93b16eb71bc20bb6e25bc774b9d05606c16b6
Daan <daan.brocatus@outlook.com>
2024-05-12 15:30:36 +0200

    Add empty plant details view and implement navigation

---
+++ BloomBuddyMobile/View/Plant/PlantDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PlantDetailView.swift
+//  BloomBuddyMobile
+//
+//  Created by Daan Brocatus on 12/05/2024.
+//
+
+import SwiftUI
+
+struct PlantDetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PlantDetailView()
+}
