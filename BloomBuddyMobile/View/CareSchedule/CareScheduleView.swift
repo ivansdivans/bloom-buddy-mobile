3f2da38ea3635eae8c54b018c8022b346168b5d7
Ivans Mihailovs <mihailovs.ivans@gmail.com>
2024-05-24 13:57:17 +0200

    Change if-else for content unavailable view

--- BloomBuddyMobile/View/CareSchedule/CareScheduleView.swift
+++ BloomBuddyMobile/View/CareSchedule/CareScheduleView.swift
@@ -12,30 +12,31 @@ struct CareScheduleView: View {
     @State var screenSize: CGSize = .zero
     
     var body: some View {
-        if viewModel.careSchedule.plants.isEmpty {
-            ContentUnavailableView(label: {
-                Label("You don't have care schedule yet", systemImage: "exclamationmark.triangle")
-            }, actions: {
-                Button(action: {
-                    // Actions to create a new care schedule
-                }, label: {
-                    Text("Create care schedule")
-                })
-                .buttonStyle(.borderedProminent)
-                .padding(.top, 30)
-            })
-        } else {
-            VStack(spacing: 0) {
-                Text(Constants.Texts.careScheduleTitle)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                HStack {
-                    Text(viewModel.getCurrentMonthAndYear())
-                }
+        VStack(spacing: 0) {
+            Text(Constants.Texts.careScheduleTitle)
+                .font(.largeTitle)
+                .fontWeight(.bold)
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 10)
-                
+            HStack {
+                Text(viewModel.getCurrentMonthAndYear())
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.bottom, 10)
+            
+            if viewModel.careSchedule.plants.isEmpty {
+                ContentUnavailableView(label: {
+                    Label("You don't have \ncare schedule yet", systemImage: "exclamationmark.triangle")
+                }, actions: {
+                    Button(action: {
+                        // open sheet
+                        // select plant
+                    }, label: {
+                        Text("Create care schedule")
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .padding(.top, 30)
+                })
+            } else {
                 WeekCalendarView(viewModel: viewModel, screenSize: screenSize)
                     .padding(.bottom, 10)
                 
@@ -44,15 +45,15 @@ struct CareScheduleView: View {
                 
                 TodoListView(viewModel: viewModel)
             }
-            .padding(10)
-            .overlay(
-                GeometryReader { proxy in
-                    Color.clear.preference(key: SizePreferenceKey.self, value: proxy.size)
-                }
-            )
-            .onPreferenceChange(SizePreferenceKey.self) { value in
-                screenSize = value
+        }
+        .padding(10)
+        .overlay(
+            GeometryReader { proxy in
+                Color.clear.preference(key: SizePreferenceKey.self, value: proxy.size)
             }
+        )
+        .onPreferenceChange(SizePreferenceKey.self) { value in
+            screenSize = value
         }
     }
 }
