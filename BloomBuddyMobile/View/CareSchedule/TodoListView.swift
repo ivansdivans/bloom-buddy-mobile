5ee36285ad293aacbc0df9a641e3da6e8627058f
Daan <daan.brocatus@outlook.com>
2024-05-28 15:08:19 +0200

    Fix todolist view

--- BloomBuddyMobile/View/CareSchedule/TodoListView.swift
+++ BloomBuddyMobile/View/CareSchedule/TodoListView.swift
@@ -11,64 +11,53 @@ struct TodoListView: View {
     @ObservedObject var viewModel: CareScheduleViewModel
     
     var body: some View {
-        // TODO: WIP
-        ForEach(viewModel.careSchedule.todos) { todo in
-            VStack {
-                Text(todo.title)
-                Text(String(describing: !todo.isDone))
-                Text("Due date is \(String(describing: todo.dueDate))")
-                Text("Selected date is \(String(describing: viewModel.selectedDate))")
-                Text("areSameDay: \(viewModel.areSameDay(todo.dueDate, viewModel.selectedDate))")
+        VStack(alignment: .leading, spacing: 10) {
+            Text(Constants.Texts.careScheduleTodo)
+                .font(.headline)
+            ForEach(viewModel.careSchedule.todos.filter {
+                !$0.isDone && Calendar.current.isDate($0.dueDate, inSameDayAs: viewModel.selectedDate)
+            }) { todo in
+                Button(action: {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        viewModel.toggleTodo(todo)
+                    }
+                }, label: {
+                    HStack(spacing: 0) {
+                        Image(systemName: "square")
+                            .foregroundColor(.primary)
+                            .padding(.trailing, 10)
+                        Text(todo.title)
+                            .foregroundStyle(Color.primary)
+                    }
+                })
             }
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.bottom, 20)
         
-//        VStack(alignment: .leading, spacing: 10) {
-//            Text(Constants.Texts.careScheduleTodo)
-//                .font(.headline)
-//            ForEach(viewModel.todos.filter {
-//                !$0.isDone && viewModel.areSameDay($0.dueDate, viewModel.selectedDate)
-//            }) { todo in
-//                Button(action: {
-//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-//                        viewModel.toggleTodo(todo)
-//                    }
-//                }, label: {
-//                    HStack(spacing: 0) {
-//                        Image(systemName: "square")
-//                            .foregroundColor(.primary)
-//                            .padding(.trailing, 10)
-//                        Text(todo.title)
-//                            .foregroundStyle(Color.primary)
-//                    }
-//                })
-//            }
-//        }
-//        .frame(maxWidth: .infinity, alignment: .leading)
-//        .padding(.bottom, 20)
-//        
-//        VStack(alignment: .leading, spacing: 10) {
-//            Text(Constants.Texts.careScheduleCompleted)
-//                .font(.headline)
-//            ForEach(viewModel.todos.filter {
-//                $0.isDone && Calendar.current.isDate($0.dueDate, inSameDayAs: viewModel.selectedDate)
-//            }) { todo in
-//                Button(action: {
-//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-//                        viewModel.toggleTodo(todo)
-//                    }
-//                }, label: {
-//                    HStack(spacing: 0) {
-//                        Image(systemName: "checkmark.square")
-//                            .foregroundColor(.primary)
-//                            .padding(.trailing, 10)
-//                        Text(todo.title)
-//                            .foregroundStyle(Color.primary)
-//                            .strikethrough()
-//                    }
-//                })
-//            }
-//        }
-//        .frame(maxWidth: .infinity, alignment: .leading)
+        VStack(alignment: .leading, spacing: 10) {
+            Text(Constants.Texts.careScheduleCompleted)
+                .font(.headline)
+            ForEach(viewModel.careSchedule.todos.filter {
+                $0.isDone && Calendar.current.isDate($0.dueDate, inSameDayAs: viewModel.selectedDate)
+            }) { todo in
+                Button(action: {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        viewModel.toggleTodo(todo)
+                    }
+                }, label: {
+                    HStack(spacing: 0) {
+                        Image(systemName: "checkmark.square")
+                            .foregroundColor(.primary)
+                            .padding(.trailing, 10)
+                        Text(todo.title)
+                            .foregroundStyle(Color.primary)
+                            .strikethrough()
+                    }
+                })
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
         
         Spacer()
     }
